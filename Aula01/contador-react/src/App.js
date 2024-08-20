3c0513437d93af4b7ef5d1ef1fcbb294114f6275
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-08-20 20:54:27 -0300

    Aula1
    Revisão

---
+++ Aula01/contador-react/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  return (
+    <div className="App">
+      <h1>Contador React</h1>
+      <p>Valor atual: {count}</p>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={decrement}>Decrementar</button>
+      <button onClick={reset}>Resetar</button>
+    </div>
+  );
+}
+export default App;
